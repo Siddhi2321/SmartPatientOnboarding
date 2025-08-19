c10879f01f1b255404958d0f015e5f938b03e409
Siddhi <siddhichhapre2128@gmail.com>
2025-08-20 02:07:10 +0530

    small changes

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,8 @@ const app = express();
 
 // Middleware
 const allowedOrigins = [
-  'https://main.dsqo2q3l6bl5h.amplifyapp.com/',
-   'https://main.dsqo2q3l6bl5h.amplifyapp.com/',
+  'https://main.dsqo2q3l6bl5h.amplifyapp.com',
+   'https://main.dsqo2q3l6bl5h.amplifyapp.com',
   'http://localhost:3000'
 ];
 
